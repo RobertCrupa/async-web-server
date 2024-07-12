526617dbc5b516368191058afc4c0cf555354494
Robert Crupa <robertcrupa@outlook.com>
2024-07-12 11:21:01 +0100

    Added support for postgres database

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # async-web-server
 🚀 A simple ASGI webserver 
+
+## Setting up Postgres docker database
+
+1. Download the images `docker pull postgres`
+2. Run with `docker run --name pg-container -p 8888:5432 -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -e POSTGRES_DB=mydb -d postgres`
--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from aiohttp import web
-from datetime import datetime
 from aiohttp.web_request import Request
 from aiohttp.web_response import Response
+from aiohttp.web_app import Application
+from datetime import datetime
+import asyncpg
+from asyncpg import Record
+from asyncpg.pool import Pool
+from typing import List
+from typing import Dict
 
 # Define route table to store endpoints
 routes = web.RouteTableDef()
+
+# Used to access our database via the application
+DB_KEY = 'database'
+
+async def create_database_pool(app: Application) -> None:
+    print('Creating database')
+
+    pool : Pool = await asyncpg.create_pool(host='127.0.0.1',
+                                           port=8888,
+                                           user='user',
+                                           password='password',
+                                           database='mydb',
+                                           min_size=6,
+                                           max_size=6)
+    app[DB_KEY] = pool
+
+async def destroy_database_pool(app: Application) -> None:
+    print('Destroying database')
+
+    pool : Pool = app[DB_KEY]
+    await pool.close()
 
 # Decorator allows this function to run when get request is sent to endpoint by a client
 @routes.get('/time')
@@ -23,6 +50,10 @@ async def time(request: Request) -> Response:
 
 # Creates out web application
 app = web.Application()
+
+# Register DB connection to be aquired and released with application lifetime
+app.on_startup.append(create_database_pool)
+app.on_cleanup.append(destroy_database_pool)
 
 # Registers our route with the app
 app.add_routes(routes)
