c0017e163d8650a7e9c5f7e60dfd72f55f3f27d1
Robert Crupa <robertcrupa@outlook.com>
2024-07-12 13:32:18 +0100

    Added data to database and endpoints to get and post products

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,8 @@
 import asyncpg
 from aiohttp.web_app import Application
 from asyncpg.pool import Pool
+from random import randint, sample
+from typing import List, Tuple, Union
 
 
 # Used to access our database via the application
@@ -28,3 +30,35 @@ async def destroy_database_pool(app: Application):
     print('Destroying database pool.')
     pool: Pool = app[DB_KEY]
     await pool.close()
+
+def generate_brand_names(words: List[str]) -> List[Tuple[Union[str, ]]]:
+    return [(words[index],) for index in sample(range(100), 100)]
+
+
+async def insert_brands(common_words, connection) -> int:
+    brands = generate_brand_names(common_words)
+    insert_brands = "INSERT INTO brand VALUES(DEFAULT, $1)"
+    return await connection.executemany(insert_brands, brands)
+
+def gen_products(common_words: List[str],
+                 brand_id_start: int,
+                 brand_id_end: int,
+                 products_to_create: int) -> List[Tuple[str, int]]:
+    products = []
+    for _ in range(products_to_create):
+        description = [common_words[index] for index in sample(range(1000), 10)]
+        brand_id = randint(brand_id_start, brand_id_end)
+        products.append((" ".join(description), brand_id))
+    return products
+
+
+def gen_skus(product_id_start: int,
+             product_id_end: int,
+             skus_to_create: int) -> List[Tuple[int, int, int]]:
+    skus = []
+    for _ in range(skus_to_create):
+        product_id = randint(product_id_start, product_id_end)
+        size_id = randint(1, 3)
+        color_id = randint(1, 2)
+        skus.append((product_id, size_id, color_id))
+    return skus
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from asyncpg.pool import Pool
 from typing import List
 from typing import Dict
 from database.commands import *
-from database import create_database_pool, destroy_database_pool
+from database import create_database_pool, destroy_database_pool, gen_products, gen_skus, insert_brands
+from faker import Faker
 import asyncio
 
 # Define route table to store endpoints
@@ -17,6 +18,9 @@ routes = web.RouteTableDef()
 
 # Used to access our database via the application
 DB_KEY = 'database'
+
+# faker used to add items to database
+fake = Faker('it_IT')
 
 # Decorator allows this function to run when get request is sent to endpoint by a client
 @routes.get('/time')
@@ -32,6 +36,54 @@ async def time(request: Request) -> Response:
 
     # returns formatted result with a 200 status code and json content type
     return web.json_response(result)
+
+@routes.post('/product')
+async def create_product(request: Request) -> Response:
+    PRODUCT_NAME = 'product_name'
+    BRAND_ID = 'brand_id'
+
+    if not request.can_read_body:
+        raise web.HTTPBadRequest()
+
+    body = await request.json()
+
+    if PRODUCT_NAME in body and BRAND_ID in body:
+        db = request.app[DB_KEY]
+        await db.execute('''INSERT INTO product(product_id, 
+                                                product_name, 
+                                                brand_id) 
+                                                VALUES(DEFAULT, $1, $2)''',
+                         body[PRODUCT_NAME],
+                         int(body[BRAND_ID]))
+        return web.Response(status=201)
+    else:
+        raise web.HTTPBadRequest()
+    
+@routes.get('/products/{id}')
+async def get_product(request: Request) -> Response:
+    try:
+        str_id = request.match_info['id'] #A
+        product_id = int(str_id)
+
+        query = \
+            """
+            SELECT
+            product_id,
+            product_name,
+            brand_id
+            FROM product
+            WHERE product_id = $1
+            """
+
+        connection: Pool = request.app[DB_KEY]
+        result: Record = await connection.fetchrow(query, product_id) #B
+
+        if result is not None: #C
+            return web.json_response(dict(result))
+        else:
+            raise web.HTTPNotFound()
+    except ValueError:
+        raise web.HTTPBadRequest()
 
 # Creates out web application
 app = web.Application()
@@ -59,14 +111,36 @@ async def main():
                   SIZE_INSERT,
                   COLOR_INSERT]
 
+    '''
     print('Creating the product database...')
     for statement in statements:
         status = await connection.execute(statement)
         print(status)
     print('Finished creating the product database!')
+    '''
+
+    # Fill database
+    common_words = [fake.word() for _ in range(1000)]
+    common_names = [fake.name() for _ in range(1000)]
+
+    await insert_brands(common_words, connection)
+
+    product_tuples = gen_products(common_names,
+                                  brand_id_start=1,
+                                  brand_id_end=100,
+                                  products_to_create=1000)
+    await connection.executemany("INSERT INTO product VALUES(DEFAULT, $1, $2)",
+                                 product_tuples)
+
+    sku_tuples = gen_skus(product_id_start=1,
+                          product_id_end=1000,
+                          skus_to_create=100000)
+    await connection.executemany("INSERT INTO sku VALUES(DEFAULT, $1, $2, $3)",
+                                 sku_tuples)
+
     await connection.close()
 
 
-# asyncio.run(main())
+#asyncio.run(main())
 
 web.run_app(app)
